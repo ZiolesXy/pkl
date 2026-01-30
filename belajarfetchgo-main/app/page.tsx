8edecb7bdc11d5e83e74dbee07370c2b94c20faf
EiyuSyaa <abrilliantp738@gmail.com>
2026-01-30 11:30:12 +0700

    creat

--- belajarfetchgo-main/app/page.tsx
+++ belajarfetchgo-main/app/page.tsx
@@ -5,18 +5,8 @@ import { Card } from "@/components/ui/card"
 export default async function UsersPage() {
 
   return (
-    <>
-      <div>
-      <Card>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, blanditiis dolor repudiandae fugit minus dolorem sapiente dolore assumenda reiciendis totam perspiciatis neque praesentium aliquam illum illo voluptate tempora voluptatem asperiores.</p>
-      </Card>
-    </div>
-    <div>
-    <Link href="/users-axios">Fetch With Axios</Link>
-    <Suspense fallback={<p>Loading...</p>}>
-    <CardTable />
-    </Suspense>
-    </div>
-    </>
+   <div>
+    <CardTable/>
+   </div>
   )
 }
